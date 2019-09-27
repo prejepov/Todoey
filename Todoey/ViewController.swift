7db0fb0c28ee8ff6c82ef2f77dc09d7aa2f5e9bc
parahat <prejepow@gmail.com>
2019-09-27 11:02:56 -0500

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by rejepov on 9/27/19.
+//  Copyright © 2019 Parahat Rejepov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
